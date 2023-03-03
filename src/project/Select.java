db444b268e24e0d58880b133a478a5125b39ef65
issaali91 <issa.alali.991@gmail.com>
2023-03-03 01:28:31 +0100

    Hotel Management System

---
+++ src/project/Select.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project;
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author issaa
+ */
+public class Select {
+
+    public static ResultSet getData(String Query) {
+        Connection con = null;
+        Statement st = null;
+        ResultSet rs = null;
+        try {
+            con = ConnectionProvider.getCon();
+            st = con.createStatement();
+            rs = st.executeQuery(Query);
+            return rs;
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        }
+
+    }
+}
